fa2ddf3bbf85b64cb8fd0256698a608d8715a41f
Sachin Patel <sachb52@gmail.com>
2014-03-30 18:01:20 -0400

    finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/signup',   to: 'users#new',   via: 'get'
 
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
