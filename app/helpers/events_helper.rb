2b8ac7033535707926de68a1483739d6b8ebd475
Sachin Patel <sachb52@gmail.com>
2014-04-18 20:07:03 -0400

    time

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,11 @@
 module EventsHelper
 	def check(event)
+		Time.zone =  'Eastern Time (US & Canada)'
 		event.end_time < Time.now
 	end
 
 	def day(event)
+		Time.zone =  'Eastern Time (US & Canada)'
 		x = event.start_date
 		y = Time.now
 		x.strftime("%A, %b %e") == y.strftime("%A, %b %e")
