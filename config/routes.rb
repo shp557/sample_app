3b9e27d28c2aa2eb995567cdf115b7694188c5ca
Sachin Patel <sachb52@gmail.com>
2014-04-24 13:24:02 -0400

    background

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/signup',   to: 'users#new',   via: 'get'
   match '/signin',   to: 'sessions#new',   via: 'get'
@@ -12,7 +13,7 @@ SampleApp::Application.routes.draw do
   match '/list', to: 'events#index', via: 'get'
   match '/calendar', to: 'events#calendar', via: 'get'
 
-  root 'events#calendar'
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
