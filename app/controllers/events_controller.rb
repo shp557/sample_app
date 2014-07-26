0765221d9bcc7410b8b7bdc5d6aed296e3578bcb
Sachin Patel <sachb52@gmail.com>
2014-07-25 22:45:21 -0400

    new update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,7 +70,7 @@ class EventsController < ApplicationController
 	def update
 		@event = Event.find(params[:id])
 		if @event.update_attributes(event_params)
-		  flash[:success] = "Event updated"
+		  flash[:success] = "Event updated."
 		  redirect_to user_path(current_user)
 		else
 		  render 'edit'
